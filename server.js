ca19082b67d2c4ae438b02b33b48ab70bf58cbd1
colledwards <colledwards@expediagroup.com>
2023-03-06 09:27:04 -0800

    added musician get endpoint

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const port = 3000;
 
 //TODO
 
+app.get("/musicians", async (request, response) => {
+    results = await Musician.findAll();
+
+    response.json(results);
+})
+
 app.listen(port, () => {
     sequelize.sync();
     console.log(`Listening on port ${port}`)
